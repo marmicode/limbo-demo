89a192c0015df9d7f7d7b30aca118f0a91cf7ae2
Younes Jaaidi <yjaaidi@gmail.com>
2019-11-29 18:06:07 +0100

    🚧 add items to cart

--- src/app/cart/cart.spec.ts
+++ src/app/cart/cart.spec.ts
@@ -14,11 +14,12 @@ function createItem(name: string, price: number): Item {
  * @deprecated 🚧 Work in progress.
  */
 class Cart {
+  private _items: Item[] = [];
   /**
    * @deprecated 🚧 Work in progress.
    */
   addItem(item: Item) {
-    throw new Error('🚧 work in progress!');
+    this._items = [...this._items, item];
   }
 }
 
@@ -27,8 +28,8 @@ describe('Cart', () => {
     const sandwich = createItem('Double Beurre', 6);
     const burger = createItem('Burger', 12);
     const cart = new Cart();
-    // cart.addItem(sandwich);
-    // cart.addItem(burger);
+    cart.addItem(sandwich);
+    cart.addItem(burger);
     // expect(cart.getItems()).toEqual([sandwich, burger])
   });
 });
