8775e23e5173b73bfe08da3f3140a9039faec74a
Younes Jaaidi <yjaaidi@gmail.com>
2019-11-29 18:02:38 +0100

    🚧 add items to cart

--- src/app/cart/cart.spec.ts
+++ src/app/cart/cart.spec.ts
@@ -1,7 +1,7 @@
 describe('Cart', () => {
   xit('🚧 should add items', () => {
-    // @todo create sandwich with price 6
-    // @todo create burger with price 12
+    // const sandwich = createItem('Double Beurre', 6);
+    // const burger = createItem('Burger', 12);
     // @todo create cart
     // @todo cart.addItem(sandwich)
     // @todo cart.addItem(burger)
