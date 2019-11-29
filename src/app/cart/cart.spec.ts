5d458addfdb66985c3e0b4bf92ddcb44f50df2dd
Younes Jaaidi <yjaaidi@gmail.com>
2019-11-29 18:04:26 +0100

    🚧 add items to cart

--- src/app/cart/cart.spec.ts
+++ src/app/cart/cart.spec.ts
@@ -1,7 +1,19 @@
+interface Item {
+  name: string;
+  price: number;
+}
+
+function createItem(name: string, price: number): Item {
+  return {
+    name,
+    price
+  };
+}
+
 describe('Cart', () => {
   xit('🚧 should add items', () => {
-    // const sandwich = createItem('Double Beurre', 6);
-    // const burger = createItem('Burger', 12);
+    const sandwich = createItem('Double Beurre', 6);
+    const burger = createItem('Burger', 12);
     // const cart = new Cart()
     // cart.addItem(sandwich)
     // cart.addItem(burger)
