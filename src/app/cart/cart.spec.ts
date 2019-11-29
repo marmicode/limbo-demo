ce90c80a450b71a7b1484db05433a620a8c79a51
Younes Jaaidi <yjaaidi@gmail.com>
2019-11-29 18:03:42 +0100

    🚧 add items to cart

--- src/app/cart/cart.spec.ts
+++ src/app/cart/cart.spec.ts
@@ -2,9 +2,9 @@ describe('Cart', () => {
   xit('🚧 should add items', () => {
     // const sandwich = createItem('Double Beurre', 6);
     // const burger = createItem('Burger', 12);
-    // @todo create cart
-    // @todo cart.addItem(sandwich)
-    // @todo cart.addItem(burger)
-    // @todo check cart.getItems() equals [sandwich, burger]
+    // const cart = new Cart()
+    // cart.addItem(sandwich)
+    // cart.addItem(burger)
+    // expect(cart.getItems()).toEqual([sandwich, burger])
   });
 });
